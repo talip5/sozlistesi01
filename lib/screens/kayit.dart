bcb9a69080c19d3c9d4299f62fc248185abc2d2e
talip5 <talipefe35@gmail.com>
2021-09-19 09:29:34 +0300

    19.09.2021 version:01

--- lib/screens/kayit.dart
+++ lib/screens/kayit.dart
@@ -12,6 +12,29 @@ class Kayit extends StatefulWidget {
 class _MyAppState extends State<Kayit> {
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    //myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    //myFocusNode.dispose();
+    super.dispose();
+  }
+
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName= TextEditingController();
+  double sonuc = 0;
+
+  _dismissDialog() {
+    Navigator.pop(context);
+  }
+
+
   Future<QuerySnapshot> getData() async {
     QuerySnapshot querySnapshot =
     await firebaseFirestore.collection('data').get();
@@ -27,13 +50,27 @@ class _MyAppState extends State<Kayit> {
 
   }
 
+  void hesapla() {
+    setState(() {
+      sonuc = double.parse(firstName.text) /
+          (double.parse(lastName.text) * double.parse(lastName.text));
+      print(sonuc);
+    });
+  }
+  Future<void> addTextData() async{
+    FirebaseFirestore firebaseFirestore=FirebaseFirestore.instance;
+    CollectionReference database=await firebaseFirestore.collection('data');
+    //DocumentReference documentReference=database.doc('4');
+    //documentReference.set({'name':'kemal'});
+    database.add({'name':firstName.text});
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Silme'),
+          title: Text('Kayıt Girişi'),
         ),
         body: Column(
           children: [
@@ -64,11 +101,61 @@ class _MyAppState extends State<Kayit> {
                 }),
             Column(
               children: [
+                TextField(
+                  enabled: true,
+                  autofocus: true,
+                  focusNode: myFocusNode,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    labelText: "firstName",labelStyle: TextStyle(fontSize: 25.0),
+                  ),
+                  controller: firstName,
+                  style: TextStyle(fontSize: 30.0),
+                  //keyboardType: TextInputType.number,
+                ),
+                TextField(
+                  enabled: true,
+                  autofocus: false,
+                 // style: TextStyle(height: 3.0, fontWeight: FontWeight.bold,fontSize: 30.0),
+                  style: TextStyle(fontSize: 30.0),
+                  cursorWidth: 5.0,
+                  //focusNode: myFocusNode,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    labelText: "lastName",labelStyle: TextStyle(fontSize: 25.0),),
+                    controller: lastName,
+                  //keyboardType: TextInputType.number,
+                  //readOnly: true,
+                  //obscureText: true,
+                ),
+
                 ElevatedButton(
                     child: Text('Kayıt Girişi',style: TextStyle(fontSize: 30.0)),
                     onPressed: (){
                       setState(() {
-                        addData();
+                        //addData();
+                        //hesapla();
+                        addTextData();
+                        showDialog(
+                          context: context,
+                          builder: (_) => new AlertDialog(title: Text("Text Dialog"),
+                            content: Text("Hey I'm Coflutter"),
+                            actions: <Widget>[
+                              TextButton(
+                                  onPressed: () {
+                                    _dismissDialog();
+                                  },
+                                  child: Text('Close')),
+                              TextButton(
+                                onPressed: () {
+                                  print('HelloWorld!');
+                                  _dismissDialog();
+                                },
+                                child: Text('HelloWorld!'),
+                              )
+                            ],
+                          ),
+                        );
                       });
                     }),
                 ElevatedButton(
